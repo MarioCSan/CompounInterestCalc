e435f533a5fa9a0e1f7204c59e7029946c3e65ed
Mario <canalessanchezmario@gmail.com>
2024-09-08 17:37:44 +0200

    Creation of react project, added the first component

---
+++ compound-calculator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CompoundInterest from './Components/CompounInterest'
+
+function App() {
+  return (
+    <div className="App">
+     <CompoundInterest />
+    </div>
+  );
+}
+
+export default App;
---
+++ compound-calculator/src/Components/CompounInterest.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CompoundInterest = () => {
+    const year = new Date().getFullYear()
+    const styles = {
+      landing: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: '500',
+        position: 'relative',
+        bottom: '0px',
+        zindex: 3,
+        color: '#d1bdbdd2'
+      }
+    };
+    return (
+    <>
+        <div> Hola mundo</div>
+    </>
+  )
+}
+
+export default CompoundInterest
